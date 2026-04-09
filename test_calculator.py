b51abc9d62785227f9bab35abe8cd7046da4c5f9
Urooj <2025sl93089@wilp.bits.pilani.ac.in>
2026-04-09 18:45:49 +0530

    changes 4

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
 import calculator
 assert calculator.add(2,3) == 5
-assert calculator.sub(5,3) == 2
-assert calculator.mul(2,3) == 6
-assert calculator.div(6,3) == 2
+assert calculator.subtract(5,3) == 2
+assert calculator.multiply(2,3) == 6
+assert calculator.divide(6,3) == 2
 print("All tests passed")
