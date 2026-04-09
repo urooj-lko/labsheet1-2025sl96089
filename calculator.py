ddfbe77e4a4d32b11f71160b5cbeb762e1b65d97
Urooj <2025sl93089@wilp.bits.pilani.ac.in>
2026-04-09 18:40:30 +0530

    changes 3

--- calculator.py
+++ calculator.py
@@ -1,13 +1,13 @@
 def add(a,b):
-   return a+b;
+   return a+b
 
 def multiply(a,b):
-    return a*b;
+    return a*b
 
 def subtract(a, b):
-    return a-b;
+    return a-b
 
 def divide(a, b):
     if b == 0:
         return None
-    return a / b;
+    return a / b
